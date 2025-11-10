57d74e2c8e02c1cb7011035683de0c0606f04d60
SAR-LOCAL <sar-local@localhost.localdomain>
2025-11-10 17:27:41 +0000

    built the iterator functor, needs updates and bounds checks

--- include/templatedMathObjs/tMultiVarVector.hpp
+++ include/templatedMathObjs/tMultiVarVector.hpp
@@ -5,6 +5,38 @@
 #include "../../../../MFEM_STUFF/mfem-4.7/build/include/mfem/general/device.hpp"
 #include "mfem.hpp"
 
+
+//
+// tensor-multi-iterator Functor used to
+// access flattened muli-dimensional vars
+//
+struct multiIterator{
+  const unsigned rank;
+//  unsigned Sizes[RANK];
+
+  //Constructor
+//  template<unsigned RANK>
+//  multiIterator(unsigned Sizes_[RANK]):rank(RANK){
+ //   #pragma unroll
+//    for(unsigned J=0; J<rank; J++) Sizes[J]=Sizes_[J];
+ // };
+
+  //Accessor multi-iter flattening
+  FORCE_INLINE unsigned operator()(const unsigned Iter[]){
+    unsigned L, I=0;
+    #pragma unroll
+    for(unsigned J=0; J<rank; J++){
+      L=Iter[J];
+      #pragma unroll
+//      for(unsigned K=0; K<(rank-J); K++) L = L*Sizes[K];
+      I=I+L;
+    }
+//    return I;
+    return 0;
+  };
+};
+
+
 //
 // templated vector of numbers 
 // using MFEMs memory layout
@@ -15,7 +47,7 @@ template<typename Numeric>
 struct tVarVectorMFEM{
   //Variable vector and iterator
   mfem::Memory<int> offsets;
-//mfem::Memory<multiIterator> Iters;
+  mfem::Memory<multiIterator> Iters;
   mfem::Memory<Numeric> varData;
   int size;
 
@@ -23,38 +55,16 @@ struct tVarVectorMFEM{
   tVarVectorMFEM(int size_, mfem::MemoryType mt): varData(size_, mt), size(size_){};
   ~tVarVectorMFEM(){varData.Delete();};
 
-  //Access Data
-//  FORCE_INLINE Numeric &operator()(int I) {return varData[I];};
-//  FORCE_INLINE Numeric &operator[](int I) {return varData[I];};
-};
-
-
-//
-// tensor-multi-iterator Functor used to
-// access flattened muli-dimensional
-// vars
-//
-template<unsigned RANK>
-struct multiIterator{
-  static const unsigned rank=RANK;
-  unsigned Sizes[RANK];
-
-  //Constructor
-  multiIterator(unsigned Sizes_[RANK]){
-    #pragma unroll
-    for(unsigned J=0; J<rank; J++) Sizes[J]=Sizes_[J];
-  };
-
-  //Accessor multi-iter flattening
-  FORCE_INLINE unsigned operator()(const unsigned Iter[rank]){
-    unsigned L, I=0;
-    #pragma unroll
-    for(unsigned J=0; J<rank; J++){
-      L=Iter[J];
-      #pragma unroll
-      for(unsigned K=0; K<(rank-J); K++) L = L*Sizes[K];
-      I=I+L;
-    }
-    return I;
+  //Data accessor
+  FORCE_INLINE Numeric &operator()(unsigned VarID, unsigned Iter[]) {
+    return varData[offsets[VarID] + Iters[VarID](Iter)];
   };
 };
+
+
+
+
+
+
+
+
